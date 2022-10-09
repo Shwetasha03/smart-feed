316491c9996711be296ec5ada988001ca1f1f60f
Nikhil Chaudhary <nikhilchaudhary0126@gmail.com>
2022-10-09 00:42:43 -0400

    added logic for showing tweets

--- home/views.py
+++ home/views.py
@@ -1,9 +1,14 @@
+import json
+import os
+
+import requests
 from django.contrib.auth.decorators import login_required
 
 from django.contrib import messages
 from django.shortcuts import render, redirect
 
 from django.db.utils import IntegrityError
+
 from home.models import Subscription
 
 
@@ -15,7 +20,6 @@ def subscribe_topics(request):
         selected_topics_checkbox = request.POST.getlist('checks[]')
         user_id = request.user.id
         topics = entered_topics + selected_topics_checkbox
-        print(topics)
         for value in topics:
             value = value.strip()
             if value != "":
@@ -32,9 +36,23 @@ def subscribe_topics(request):
 def get_sentiment(request):
     first_15_subs = Subscription.objects.filter(userid=request.user.id).order_by('-id').all()[:15]
     topics = [item.category for item in first_15_subs]
+    filtered_response = []
     if request.method == 'POST':
         if "topic" not in request.POST:
             messages.error(request, "Food Post failed! Please enter all details.")
             return redirect('sentiments')
         topic = request.POST["topic"]
-    return render(request, 'get_sentiment.html', {"topics": topics})
+        vibe = request.POST["vibe"]
+
+        url = os.getenv("API_URL") + topic
+        headers = {'auth': os.getenv("AUTH_TOKEN")}
+        body = {}
+
+        response = requests.post(url, json=body, headers=headers)
+        json_response = json.loads(json.JSONDecoder().decode(response.text))
+        json_response = [{'text': 'Tweet', 'sentiment': 'Sentiment'}] + json_response
+        for item in json_response:
+            if item["sentiment"] == vibe or vibe == "ALL":
+                filtered_response.append(item)
+
+    return render(request, 'get_sentiment.html', {"topics": topics, "results": filtered_response})
